41541ef9ad5dbe8386567ec6791b335bb72b0f87
Adrian <adriankubinyete@outlook.com>
2023-03-28 20:18:26 -0300

    gitignore update (login.html)

--- core.py
+++ core.py
@@ -7,13 +7,15 @@ import asyncio
 def main():
     log = venLog()
     log.info("INICIANDO")
+    
     venusdb = VenusDB(
         db_host=lerArquivo("secret/venus_mariadb_host.txt"), 
         db_user=lerArquivo("secret/venus_mariadb_usuario.txt"), 
         db_pass=lerArquivo("secret/venus_mariadb_senha.txt", encrypt_sha1=True),
         db_database=lerArquivo("secret/venus_mariadb_database.txt")
         )
-    venusws = VenusWS(venusdb) # Inicio o WS conectado a este DB
+
+    venusws = VenusWS(venusdb)
     venusws.app.run(host='0.0.0.0', debug=True)
         
 if __name__ == "__main__":
